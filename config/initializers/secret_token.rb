a37ee8977d637a416a1f2f73595ab8836fda7432
Pallavi <pallavi.mathane@sv.cmu.edu>
2013-09-07 13:09:11 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtHeart::Application.config.secret_key_base = '870e16ad4fa56f591009f04f03bd5e37939a8d8adc2b029ce9b5c8f9962a8af650a95048193d6a08c8edef4bc9e54163e484c4c282b68cb28cc8b96e73248405'
